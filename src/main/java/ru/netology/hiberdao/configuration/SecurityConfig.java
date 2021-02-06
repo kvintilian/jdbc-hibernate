ed87280e09220b5c6ddd863b6e3579d103c4e435
Andrey Zemlyachev <kvintilian@gmail.com>
2021-02-06 17:15:42 +0300

    Done Задача Безопасное приложение

--- src/main/java/ru/netology/hiberdao/configuration/SecurityConfig.java
+++ src/main/java/ru/netology/hiberdao/configuration/SecurityConfig.java
@@ -3,30 +3,26 @@ package ru.netology.hiberdao.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("12345")).roles("READ");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/persons/save**", "/persons/delete-by-id", "/persons/find-all").authenticated().and().formLogin()
-            .and().authorizeRequests().anyRequest().permitAll();
-//    http.formLogin().and().authorizeRequests().antMatchers("/by-city").permitAll();
-//            .and().authorizeRequests().anyRequest().authenticated();
-//    http.authorizeRequests().antMatchers("/by-city").authenticated().and().formLogin();
-  }
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    super.configure(web);
-//    web.ignoring().antMatchers("/by-age-less**");
+    http
+            .authorizeRequests().antMatchers("/persons/count").permitAll()
+            .and()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .formLogin();
   }
 }
